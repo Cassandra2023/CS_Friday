1849c7ff7ce813e2ec598453f4276df4b1dc994e
Cassandra2023 <134661696+Cassandra2023@users.noreply.github.com>
2023-05-26 11:12:44 +1000

    Update PortScannerThread.py

--- PortScannerThread.py
+++ PortScannerThread.py
@@ -10,6 +10,7 @@ import socket
 # This is another comment from David
 
 # Cass to put a comment under here:
+# Yes I shall put a comment under here
 
 # a print_lock is what is used to prevent "double" modification of shared variables.
 # this is used so while one thread is using a variable, others cannot access
